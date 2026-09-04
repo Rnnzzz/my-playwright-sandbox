a9db30fc98b11bf64814f1456f0d5e84bb366f36
Rnnzzz <mirafloresmarkrenzo@gmail.com>
2026-09-04 23:02:05 +0800

    increase navigation timeout to 60sec

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     actionTimeout: 15000,
-    navigationTimeout: 30000,
+    navigationTimeout: 60000,
   },
 
   projects: [
